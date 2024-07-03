6a5d0dd3bda5725302379aa80b17893301da2e2e
Wesley Lui <wesley.lui2@ucalgary.ca>
2024-07-03 13:36:53 -0600

    initial commit

---
+++ Application.java
@@ -0,0 +1,16 @@
+/*
+ * Application.java
+ * Lab01
+ * 
+ * @author Wesley Lui
+ * @version 1.0
+ */
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+	}
+
+}
