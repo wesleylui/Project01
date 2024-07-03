3c98879cdd96eb21ecbeafd1ee3c6a43197824c8
Wesley Lui <wesley.lui2@ucalgary.ca>
2024-07-03 14:02:56 -0600

    create add&factorial functions. and switch case in main

--- com/wesley/calculator/Application.java
+++ com/wesley/calculator/Application.java
@@ -19,9 +19,34 @@ public class Application {
 			Scanner scanner = new Scanner(System.in);
 			System.out.println("Enter operation (e.g., add, subtract, multiply, divide):");
 			String operation = scanner.next();
-			// Further processing based on operation
+
+			switch (operation.toLowerCase()) {
+			case "add":
+				System.out.println("Enter the first operand: ");
+				double num1 = scanner.nextDouble();
+				System.out.println("Enter the second operand: ");
+				double num2 = scanner.nextDouble();
+				System.out.println("Result: " + add(num1, num2));
+				break;
+			case "factorial":
+				System.out.println("Enter a number: ");
+				double number = scanner.nextDouble();
+				System.out.println("Result: " + factorial(number));
+				break;
+			}
 		}
+
 	}
 
-	// TODO: Define methods for calculations
+	public static double add(double a, double b) {
+		return a + b;
+	}
+
+	public static double factorial(double n) {
+		if (n <= 1) {
+			return 1;
+		} else {
+			return n * factorial(n - 1);
+		}
+	}
 }
