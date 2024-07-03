e2f6227ef9a67506a39205ca708f599ad090ad11
Wesley Lui <wesley.lui2@ucalgary.ca>
2024-07-03 13:53:17 -0600

    setup CLI scanner

--- com/wesley/calculator/Application.java
+++ com/wesley/calculator/Application.java
@@ -1,15 +1,26 @@
 /*
  * Application.java
+
  * Lab01
  * 
  * @author Wesley Lui
  */
+
 package com.wesley.calculator;
 
-public class Application {
+import java.util.Scanner;
 
+public class Application {
 	public static void main(String[] args) {
-		// TODO: Parse CLI arguments or wait for user input
+		if (args.length > 0) {
+			// Process CLI arguments
+		} else {
+			// No CLI arguments, ask for user input
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("Enter operation (e.g., add, subtract, multiply, divide):");
+			String operation = scanner.next();
+			// Further processing based on operation
+		}
 	}
 
 	// TODO: Define methods for calculations
